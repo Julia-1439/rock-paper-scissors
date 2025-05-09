8d1769f41c1e2338b24551fb309a281121b15e3d
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-05-08 21:01:09 -0700

    Create pseudocode of playRound function

--- script.js
+++ script.js
@@ -20,3 +20,43 @@ function getHumanChoice() {
     const humanChoice = prompt("Hello User, what is your choice?");
     return humanChoice;
 }
+
+
+/**
+
+FUNCTION play a round of the game
+    PASS IN computer's choice, human's choice
+
+SET humanChoice to all lower case
+
+IF computerChoice is the same as humanChoice THEN
+	PRINT message saying tie
+	PASS OUT nothing
+ENDIF
+
+SET computerWins to false
+
+CASE computerChoice OF
+    computerChoice is "rock": 
+        SET computerWins to whether humanChoice is "scissors" 
+        BREAK
+    computerChoice is "paper": 
+        SET computerWins to whether humanChoice is "rock"
+        BREAK
+    computerChoice is "scissors": 
+        SET computerWins to whether humanChoice is "paper"
+        BREAK
+ENDCASE
+
+IF computerWins THEN
+    INCREMENT computerScore
+ELSE
+    INCREMENT humanScore
+ENDIF
+
+PRINT message of the winner
+PASS OUT nothing
+
+ENDFUNCTION
+
+ */
