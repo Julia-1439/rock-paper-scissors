0b2e5099cb1719bbcddc0d89509a205e58abf032
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-05-08 20:20:09 -0700

    Add algorithm for computer's choice

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    const seed = Math.random();
+    let computerChoice;
+    if (seed < 0.33) {
+        computerChoice = 'Rock';
+    }
+    else if (0.33 <= seed && seed < 0.66) {
+        computerChoice = 'Paper';
+    } 
+    else {
+        computerChoice = 'Scissors';
+    }
+    return computerChoice;
+}
