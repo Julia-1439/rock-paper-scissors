3094159abd2d78e45511429f941819a0dc1adcc4
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-05-08 21:22:20 -0700

    Create algorithm to play one round of the game

--- script.js
+++ script.js
@@ -20,6 +20,41 @@ function getHumanChoice() {
     return prompt("Hello User, what is your choice?");
 }
 
+function playRound(computerChoice, humanChoice){
+    humanChoice = humanChoice.toLowerCase();
+    
+    // Terminate early in case of a tie
+    if (computerChoice === humanChoice){
+        console.log(`It's a tie! You both chose ${computerChoice}.`);
+        return;
+    }
+
+    // Determine winner
+    let computerWins;
+    switch (computerChoice){
+        case "rock":
+            computerWins = humanChoice === "scissors";
+            break;
+        case "paper":
+            computerWins = humanChoice === "rock";
+            break;
+        case "scissors":
+            computerWins = humanChoice === "paper";
+            break;
+    }
+
+    // Increment appropriate score and Print message
+    if (computerWins) {
+        ++computerScore;
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+    else {
+        ++humanScore;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    }
+
+    return;
+}
 
 /**
 
